43e4ccc966cd80cb288eac5111f3d565a87af916
Taylor Fausak <taylor@fausak.me>
2020-02-08 10:01:10 -0500

    Build a Docker image

--- .semaphore/build.sh
+++ .semaphore/build.sh
@@ -5,3 +5,11 @@ cache_key="stack-$( checksum stack.yaml )-$( checksum monadoc.cabal )"
 cache restore "$cache_key"
 stack --allow-different-user --system-ghc build --pedantic
 cache store "$cache_key" .stack
+cp --recursive --verbose data container
+cp --verbose "$( command -v monadoc )" container
+cd container || exit
+mkdir libs
+ldd monadoc | awk '/=>/ { print $3 }' | xargs cp --target-directory libs --verbose
+zip_file="monadoc-$SEMAPHORE_GIT_SHA.zip"
+zip --recurse-paths --verbose "$zip_file" .
+artifact push workflow --expire-in 1w "$zip_file"
