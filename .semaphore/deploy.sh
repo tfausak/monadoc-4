453ede1908e810b3d50c67d507c44373c8e3bf10
Taylor Fausak <taylor@fausak.me>
2020-02-08 10:21:33 -0500

    Build and push Docker images to ECR

--- .semaphore/deploy.sh
+++ .semaphore/deploy.sh
@@ -1,4 +1,9 @@
 #! /usr/bin/env sh
-env
-pwd
-ls
+zip_file="monadoc-$SEMAPHORE_GIT_SHA.zip"
+artifact pull workflow "$zip_file"
+unzip "$zip_file"
+eval "$( aws ecr get-login --no-include-email )"
+url=$( aws ecr describe-repositories --output 'text' --query 'repositories[0].repositoryUri' --repository-names monadoc )
+tag="$url:$SEMAPHORE_GIT_SHA"
+docker build --tag "$tag" .
+docker push "$tag"
