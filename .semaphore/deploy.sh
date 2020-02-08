8812e74a4f2d8cb010ac417b33db869cb9ba1f09
Taylor Fausak <taylor@fausak.me>
2020-02-08 10:30:04 -0500

    Build the Docker image in an empty temporary directory

--- .semaphore/deploy.sh
+++ .semaphore/deploy.sh
@@ -1,7 +1,9 @@
 #! /usr/bin/env sh
 zip_file="monadoc-$SEMAPHORE_GIT_SHA.zip"
+cd "$( mktemp --directory )" || exit
 artifact pull workflow "$zip_file"
 unzip "$zip_file"
+rm "$zip_file"
 eval "$( aws ecr get-login --no-include-email )"
 url=$( aws ecr describe-repositories --output 'text' --query 'repositories[0].repositoryUri' --repository-names monadoc )
 tag="$url:$SEMAPHORE_GIT_SHA"
